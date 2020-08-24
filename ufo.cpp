25f334df5bd22a981face22433382f19a46ae210
Elise Weir <67879336+eliseweir@users.noreply.github.com>
2020-08-24 18:02:15 -0500

    Initial upload

---
+++ ufo.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include "ufo_functions.hpp"
+
+int main() {
+
+  std::string codeword = "codecademy";
+  std::string answer = "__________";
+  int misses = 0;
+  std::vector<char> incorrect;
+  bool guess = false;
+  char letter;
+
+  greet();
+
+  while (answer != codeword && misses < 7) {
+
+    display_misses(misses);
+
+    display_status(incorrect, answer);
+
+    std::cout << "Please enter your guess: ";
+    std::cin >> letter;
+    for(int k=0; k<codeword.size(); k++) {
+      if (letter == codeword[k]) {
+        answer[k] = letter;
+        guess = true;
+      }
+    }
+    if (guess == true) {
+      std::cout << "Correct!\n";
+    }
+    else {
+      std::cout << "Incorrect! The tractor beam pulls the person in further.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+
+    guess = false;
+
+  }
+
+  end_game(answer, codeword);
+
+}
